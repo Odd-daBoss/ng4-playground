665cd08101f461caec80364f0fdb6eb5aecdd1c2
Odd daBoss <odd.daboss@sdee.co>
2017-06-06 10:13:34 +0700

    initial version - learning angular4 and git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <h1>Hello World!</h1>
+
+	<button [disabled]="buttonStatus">My Button</button>
+
+  `,
 })
+
 export class AppComponent {
-  title = 'app';
+
+	buttonStatus = false;
+
 }
